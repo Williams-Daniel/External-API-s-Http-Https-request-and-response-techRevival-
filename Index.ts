effd95ceafe888ec13c17d72ef138d50213ba0e9
Williams-Daniel <tito4bbt@gmail.com>
2024-05-02 14:40:16 +0100

    reading api

--- Index.ts
+++ Index.ts
@@ -1,15 +1,26 @@
 import http from "http"
 import https from "https"
 
-const port:number = 4455
 
+const port:number = 4455
+const URL:string =  "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 const app = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type" : "application/json"})
     const {method,url} = req
     if(method === "GET" && url === "/"){
         console.log(https)
 
-        res.end(JSON.stringify(https))
+        https.get(URL,(response)=>{
+            let body = "" 
+            response.on("data",(chunk)=>{
+                body += chunk
+            })
+
+            response.on ("end",()=>{
+                let result = JSON.parse(body).url
+                res.end(JSON.stringify(result))
+            })
+        })
     }
 })
 
