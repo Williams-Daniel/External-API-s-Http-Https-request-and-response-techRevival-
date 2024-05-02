cba057f0dbab9be7ffbe19a651ad2613a625a8bf
Williams-Daniel <tito4bbt@gmail.com>
2024-05-02 14:06:12 +0100

    creating server

--- Index.ts
+++ Index.ts
@@ -4,7 +4,13 @@ import https from "https"
 const port:number = 4455
 
 const app = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type" : "application/json"})
+    const {method,url} = req
+    if(method === "GET" && url === "/"){
+        console.log(https)
 
+        res.end(JSON.stringify(https))
+    }
 })
 
 app.listen(port,()=>{
